d984c1e2c633de7d0385796169747c948c0bbfc2
Luiz Muniz <luiz.ppiruk@gmail.com>
2024-10-15 17:36:58 +0100

    fixed per page filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -15,13 +15,10 @@ class ProductsController < ApplicationController
     country_filter = params[:country].presence
 
     sql_products = fetch_sql_products(country_filter, page, per_page)
-    mongo_products = fetch_mongo_products(country_filter, page, per_page)
-
-    combined_products = sql_products.to_a + mongo_products.to_a
     total_count = count_total_products(country_filter)
 
     render json: {
-      products: combined_products,
+      products: sql_products,
       total_pages: (total_count / per_page.to_f).ceil,
       current_page: page
     }
@@ -34,14 +31,7 @@ class ProductsController < ApplicationController
     scope.paginate(page: page, per_page: per_page)
   end
 
-  def fetch_mongo_products(country, page, per_page)
-    scope = country ? MongoProduct.where(country: country) : MongoProduct
-    scope.skip((page - 1) * per_page).limit(per_page)
-  end
-
   def count_total_products(country)
-    total_sql_count = country ? SqlProduct.where(country: country).count : SqlProduct.count
-    total_mongo_count = country ? MongoProduct.where(country: country).count : MongoProduct.count
-    total_sql_count + total_mongo_count
+    country ? SqlProduct.where(country: country).count : SqlProduct.count
   end
 end
